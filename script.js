b08a399d5c7531995d1555cea5e4ca3cd4a87185
DanilTsapok <116272494+DanilTsapok@users.noreply.github.com>
2022-11-29 17:29:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,5 +5,17 @@
     div.innerHTML = '<p>CreateElement example</p>';
     document.body.appendChild(div);
 
-
-
+    
+    function ran()
+    {let answers = ['Так', 'Ні']
+        let question = document.getElementById('question').value
+        if(  question.includes('@') || question.includes('#')||!question.includes('?'))
+        {
+            document.getElementById('answer').value = "Інше питання";
+        }
+        else 
+        {
+            document.getElementById('answer').value = answers[Math.floor(Math.random() * answers.length)];
+        }
+    }
+    
